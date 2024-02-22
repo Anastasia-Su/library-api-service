bc3ee56f47cfb39c50cab34a3d8e719debe85392
Anastasia <anastasiiya.p@gmail.com>
2024-02-22 19:08:36 +0200

    Add permissions

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import (
     Book,
@@ -10,11 +11,16 @@ from .serializers import (
     BookDetailSerializer,
 )
 
+from .permissions import (
+    IsCurrentlyLoggedIn,
+    IsAuthenticatedReadOnly,
+)
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticatedReadOnly]
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -23,3 +29,9 @@ class BookViewSet(viewsets.ModelViewSet):
             return BookDetailSerializer
 
         return BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["update", "destroy", "create"]:
+            return [IsAdminUser()]
+
+        return [IsAuthenticatedReadOnly()]
